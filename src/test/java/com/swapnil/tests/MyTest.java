9cb8706d1d441937edc69c3d9883a91eb79b1877
swapnilirnak <swapnilirnaks@gmail.com>
2025-06-28 15:07:46 +0530

    First Commit: Maven TestNG Setup

---
+++ src/test/java/com/swapnil/tests/MyTest.java
@@ -0,0 +1,16 @@
+package com.swapnil.tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class MyTest {
+
+	@Test
+	public void sampleTest() {
+		System.out.println("This is a sample test case");
+		Assert.assertTrue(true);
+	}
+}
+
+
